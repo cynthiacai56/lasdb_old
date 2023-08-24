15d3e63678efa268eb8aa739f0484c9b8392fd7a
cynthiacai <cynthycai@outlook.com>
2023-08-24 15:34:11 +0200

    my change

--- importer.py
+++ importer.py
@@ -38,34 +38,5 @@ def main():
         parser.print_help()
 
 
-def test():
-    input_path = 'E:\\Geomatics\\GEO2020 Thesis\\data\\'
-    input_filename = 'C_37EN1_500m.las'
-    ratio = 0.7
-    engine_key = 'postgresql://postgres:050694@localhost:5432/lasdb_500_70'
-    meta_id = 1
-
-    one_file_importer(meta_id, input_path, input_filename, ratio, engine_key)
-
-    '''
-    #test = [[0.7, 'postgresql://postgres:050694@localhost:5432/lasdb_1_70']]
-
-    #test = [#[0.9, 'postgresql://postgres:050694@localhost:5432/lasdb_4_dec_90'],
-            [0.8, 'postgresql://postgres:050694@localhost:5432/lasdb_5_dec_80'],
-            [0.7, 'postgresql://postgres:050694@localhost:5432/lasdb_5_dec_70'],
-            [0.6, 'postgresql://postgres:050694@localhost:5432/lasdb_5_dec_60']
-            #[0.5, 'postgresql://postgres:050694@localhost:5432/lasdb_4_dec_50']
-            ]
-
-    for ratio, engine_key in test:
-        start_time = time.time()
-        one_file_importer(input_path, input_filename, ratio, engine_key)
-
-        runtime = time.time() - start_time
-        print(ratio, "Runtime:", runtime, "seconds")
-    '''
-
-
 if __name__ == "__main__":
-    #main()
-    test()
+    main()
