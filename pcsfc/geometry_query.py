6093374693c394fc7c714055c2f3d16378d4c789
Yuduan Cai <81803109+cynthiacai56@users.noreply.github.com>
2023-09-08 13:25:42 +0200

    Update geometry_query.py

--- pcsfc/geometry_query.py
+++ pcsfc/geometry_query.py
@@ -2,17 +2,7 @@ import numpy as np
 from scipy.spatial import KDTree
 from shapely.geometry import Polygon, Point
 
-'''
-Second-filter
-There are four geometrical searches:
-
-1. Bounding box
-2. Circle
-3. Polygon
-4. Nearest neighbour
-'''
-
-def bbox_filter(x_min, x_max, y_min, y_max, points):
+def bbox_filter(points, x_min, x_max, y_min, y_max):
     points_within_bbox = points[
     (points[:, 0] >= x_min) &
     (points[:, 0] <= x_max) &
@@ -22,7 +12,7 @@ def bbox_filter(x_min, x_max, y_min, y_max, points):
     return points_within_bbox
 
 
-def circle_filter(center, radius, points):
+def circle_filter(points, center, radius):
     pts_2d = np.array(points)[:, :2]
     tree = KDTree(pts_2d)
     indices = tree.query_ball_point(center, radius)
@@ -30,12 +20,14 @@ def circle_filter(center, radius, points):
     return points_within_circle
 
 
-def polygon_filter(polygon_vertices, points):
+def polygon_filter(points, polygon_vertices, hole_vertices=[]):
     polygon = Polygon(polygon_vertices)
+    if len(hole_vertices) >= 3:
+        hole_polygon = Polygon(hole_vertices)
+        polygon = polygon.difference(hole_polygon)
+        
     points_within_polygon = []
-
     for pt in points:
-        print(type(pt), pt)
         point_obj = Point(pt[0], pt[1])
         if polygon.contains(point_obj):
             points_within_polygon.append(pt)
