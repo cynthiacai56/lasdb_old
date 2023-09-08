5dc66330084d0dd8fb9437597832a2cf9a707daf
Yuduan Cai <81803109+cynthiacai56@users.noreply.github.com>
2023-09-08 08:39:42 +0200

    Update query.py

--- query.py
+++ query.py
@@ -14,8 +14,8 @@ def bbox_search(args):
     bbox_filter = GeometryFilter(GeometryFilter.MODE_BBOX, constr, head_len, tail_len, db_url)
     results = bbox_filter.query()
 
-    print(len(results))
-    print(results)
+    print('The number of results:', len(results))
+    print('The results:', results)
 
     end_time = time.time()
     print('The run time:', end_time-start_time)
@@ -30,8 +30,8 @@ def circle_search(args):
     circle_filter = GeometryFilter(GeometryFilter.MODE_CIRCLE, constr, head_len, tail_len, db_url)
     results = circle_filter.query()
 
-    print(len(results))
-    print(results)
+    print('The number of results:', len(results))
+    print('The results:', results)
 
     end_time = time.time()
     print('The run time:', end_time-start_time)
@@ -46,8 +46,8 @@ def polygon_search(args):
     polygon_filter = GeometryFilter(GeometryFilter.MODE_POLYGON, constr, head_len, tail_len, db_url)
     results = polygon_filter.query()
 
-    print(len(results))
-    print(results)
+    print('The number of results:', len(results))
+    print('The results:', results)
 
     end_time = time.time()
     print('The run time:', end_time-start_time)
@@ -109,8 +109,8 @@ def main():
     if hasattr(args, "func"):
         start_time = time.time()
         args.func(args)
-        run_time = time.time() - start_time()
-        print("The running time:", run_time)
+        run_time = time.time() - start_time
+        print("The total running time:", run_time)
     else:
         parser.print_help()
 
